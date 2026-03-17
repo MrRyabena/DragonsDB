9db18179d7754f7b471001ced1d9549c656a7111
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-03-17 04:47:41 +0300

    Update API.

--- src/storage/FileStorage.java
+++ src/storage/FileStorage.java
@@ -1,66 +1,72 @@
 package storage;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import dragon.Dragon;
-
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Set;
-import java.util.stream.Stream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.file.attribute.FileTime;
+import java.util.Date;
 
 /**
- * Хранилище коллекции Dragon в JSON-файле. Использует библиотеку Gson; объем кода заметно меньше по
- * сравнению с ручным парсингом, при этом формат остается стандартным и расширяемым. Метаданные
- * коллекции (время создания/изменения) не сериализуются, поскольку легко читаются из файловой
+ * Хранилище коллекции Dragon в JSON-файле. Использует библиотеку Gson; объем
+ * кода заметно меньше по
+ * сравнению с ручным парсингом, при этом формат остается стандартным и
+ * расширяемым. Метаданные
+ * коллекции (время создания/изменения) не сериализуются, поскольку легко
+ * читаются из файловой
  * системы через Files.getLastModifiedTime(...).
  */
 public class FileStorage implements Storage {
-    private final Path filename;
-
-    /* Gson and type for (de)serialization */
-    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private final Type collectionType = new TypeToken<Set<Dragon>>() {}.getType();
 
     public FileStorage(Path filename) {
         this.filename = filename;
     }
 
-    // @Override
-    // public void save(collection.Collection collection) throws IOException {
-    //     try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename.toFile()))) {
-    //         writer.write(gson.toJson(collection.getBuffer(), collectionType));
-    //     }
-    // }
-
     @Override
-    public void save(InputStream stream) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename.toFile()))) {
-            stream.forEachOrdered();
+    public void save(InputStream stream) {
+        try (var reader = new InputStreamReader(stream, core.Defaults.CHARSET);
+                var writer = new BufferedWriter(new FileWriter(filename.toFile()))) {
+            reader.transferTo(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
-    public OutputStream load() throws IOException {
-        if (!Files.exists(filename)) {
-            return Stream.empty();
-        }
+    public OutputStream load() {
 
-        try (var reader =
-                new BufferedReader(new InputStreamReader(new FileInputStream(filename.toFile())))) {
+        var baos = new ByteArrayOutputStream();
 
-            return reader.lines();
+        try (var reader = new InputStreamReader(new FileInputStream(filename.toFile()));
+                var writer = new OutputStreamWriter(baos, core.Defaults.CHARSET)) {
+            reader.transferTo(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return baos;
     }
+
+    @Override
+    public Date getDateCreated() {
+        try {
+            new Date(((FileTime) (Files.getAttribute(filename, "creationTime"))).toMillis());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Date getDateModified() {
+        return new Date(filename.toFile().lastModified());
+    }
+
+    private final Path filename;
 }
