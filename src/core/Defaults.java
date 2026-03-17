4ea6fa2081354366cce47f0dc4f74b7fae613758
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-03-17 04:45:32 +0300

    Created for constants.

---
+++ src/core/Defaults.java
@@ -0,0 +1,9 @@
+package core;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public final class Defaults {
+    public static final String STORAGE_PATH = "data.json";
+    public static final Charset CHARSET = StandardCharsets.UTF_8;
+}
