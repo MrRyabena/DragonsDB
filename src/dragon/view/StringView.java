2d867e7b1307f3d46d12fd303fc8369825922391
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-03-17 04:47:02 +0300

    Creted StringView.

---
+++ src/dragon/view/StringView.java
@@ -0,0 +1,65 @@
+package dragon.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.stream.Stream;
+
+import dragon.Dragon;
+
+public class StringView implements View {
+
+    @Override
+    public InputStream toView(Stream<Dragon> stream) {
+        StringBuilder sb = new StringBuilder();
+
+        stream.forEach(dragon -> {
+            // Root-level fields
+            appendField(sb, "id", String.valueOf(dragon.getId()), 0);
+            appendField(sb, "name", dragon.getName(), 0);
+            appendField(sb, "creationDate", String.valueOf(dragon.getCreationDate()), 0);
+            appendField(sb, "age", String.valueOf(dragon.getAge()), 0);
+            appendField(sb, "weight", String.valueOf(dragon.getWeight()), 0);
+            appendField(sb, "speaking", String.valueOf(dragon.isSpeaking()), 0);
+
+            // Nested fields
+            appendField(sb, "coordinates", null, 0);
+            if (dragon.getCoordinates() != null) {
+                appendField(sb, "x", String.valueOf(dragon.getCoordinates().getX()), 1);
+                appendField(sb, "y", String.valueOf(dragon.getCoordinates().getY()), 1);
+            }
+
+            appendField(sb, "type", dragon.getType() == null ? "null" : dragon.getType().name(), 0);
+
+            appendField(sb, "head", null, 0);
+            if (dragon.getHead() != null) {
+                appendField(sb, "size", String.valueOf(dragon.getHead().getSize()), 1);
+                appendField(sb, "toothCount", String.valueOf(dragon.getHead().getToothCount()), 1);
+            }
+
+            // Separator between dragons
+            sb.append(System.lineSeparator());
+        });
+
+        return new java.io.ByteArrayInputStream(sb.toString().getBytes(core.Defaults.CHARSET));
+    }
+
+    private static void appendField(StringBuilder sb, String name, String value, int indentLevel) {
+        for (int i = 0; i < indentLevel; i++) {
+            sb.append("  ");
+        }
+
+        sb.append(name);
+
+        if (value != null) {
+            sb.append(' ').append(value);
+        }
+
+        sb.append(System.lineSeparator());
+    }
+
+    @Override
+    public Stream<Dragon> fromView(OutputStream stream) {
+        return Stream.empty();
+    }
+
+}
