b0f29fe79eccfb3b16129543d2df854497863128
MrRyabena <147092300+MrRyabena@users.noreply.github.com>
2026-03-17 06:09:23 +0300

    Completed.

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,46 @@
 import java.nio.file.InvalidPathException;
+import java.io.OutputStreamWriter;
 
-import core.Coordinates;
-import dragon.Dragon;
-import dragon.DragonHead;
-import dragon.DragonType;
+import dragon.view.JsonView;
 import storage.FileStorage;
+import ui.UI;
+import ui.TextUIHandler;
 
 public class Main {
 
     public static void main(String args[]) {
-        var collection = new collection.Collection();
 
-        collection.add(
-                new Dragon(
-                        "Smaug",
-                        new Coordinates(10.0f, 20.0f),
-                        500,
-                        10000,
-                        DragonType.FIRE,
-                        new DragonHead(2.0f, 5.0f)));
-        collection.add(
-                new Dragon(
-                        "Charlie",
-                        new Coordinates(10.0f, 20.0f),
-                        100,
-                        10000,
-                        DragonType.FIRE,
-                        new DragonHead(2.0f, 5.0f)));
+        String storage_path = null;
+        collection.Collection collection = null;
 
         try {
-            var env = System.getenv("STORAGE_PATH");
-            System.err.println(env);
-            var storage =
-                    new FileStorage(
-                            java.nio.file.FileSystems.getDefault()
-                                    .getPath(env));
-            try {
-                storage.save(collection.getStream());
-            } catch (Exception e) {
-                e.printStackTrace();
-                // TODO
-            }
+            storage_path = System.getenv("STORAGE_PATH");
         } catch (InvalidPathException e) {
             e.printStackTrace();
 
         }
+
+        if (storage_path == null) {
+            storage_path = core.Defaults.STORAGE_PATH;
+            System.err.printf("Environment variable STORAGE_PATH not found! Using default path: \"%s\"\n", storage_path);
+        }
+
+        var storage = new FileStorage(java.nio.file.FileSystems.getDefault().getPath(storage_path));
+        var jsonView = new JsonView();
+
+        
+
+        try {
+            //storage.save(jsonView.toView(collection.getStream()));
+            collection = new collection.Collection(jsonView.fromView(storage.load()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            // TODO
+        }
+
+        var ui = new UI(collection, storage, new OutputStreamWriter(System.out));
+        var textUIHandler = new TextUIHandler(ui);
+
+        textUIHandler.run();
     }
 }
