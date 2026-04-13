55cd8e0ec8841fdfe9ad06fe35615867510e28f7
Daniil Ryazanov <147092300+MrRyabena@users.noreply.github.com>
2026-04-14 02:45:29 +0300

    Update main.

--- src/LoadBalancer/src/main.cpp
+++ src/LoadBalancer/src/main.cpp
@@ -1,12 +1,22 @@
 #include <cstdint>
 #include <exception>
 #include <iostream>
+#include <memory>
 
+#include "net/PacketHandler.hpp"
 #include "net/UdpServer.hpp"
 
 int main() {
     try {
-        lb::UdpServer server("0.0.0.0", static_cast<std::uint16_t>(5000));
+    auto packetHandler = std::make_shared<lb::PacketHandler>();
+        lb::UdpServer server(
+                "0.0.0.0",
+                static_cast<std::uint16_t>(5000),
+        [packetHandler](lb::UdpServer& udpServer,
+                 const std::vector<std::uint8_t>& packet,
+                 const boost::asio::ip::udp::endpoint& remote) {
+            packetHandler->handle(udpServer, packet, remote);
+                });
         server.start();
 
         std::cout << "Press Enter to stop the load balancer..." << std::endl;
